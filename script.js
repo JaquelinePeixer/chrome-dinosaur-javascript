9443ae86801b342458c513707994a36148620649
Jaqueline Peixer <peixer.jaqueline@gmail.com>
2024-01-14 08:49:04 -0300

    game

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const time = document.getElementById("time");
-
-// exibir o tempo de duração
-//start
+const gameOver = document.getElementById("gameOver");
+let start = false;
+let cronometro;
 
 function jump() {
     if (dino.classList != "jump") {
@@ -23,12 +23,37 @@ let isAlive = setInterval(function () {
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
     //detect collision
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 50) {
         // collision
-        alert("Gamer Over!")
+        gameOver.classList.add("visible");
+        inicial();
     }
 }, 10)
 
+function timeStarted() {
+    let i = 0;
+    cronometro = setInterval(() => {
+        i++
+        let tempo = String(i).padStart(6, "0");
+        time.innerHTML = tempo;
+    }, 100);
+}
+
 document.addEventListener("keydown", function (event) {
+    if (start === false) {
+        let tempo = String(0).padStart(6, "0");
+        time.innerHTML = tempo;
+        gameOver.classList.remove("visible");
+        start = true;
+        timeStarted();
+    }
+    cactus.classList.add("animationCactus");
     jump();
 })
+
+function inicial() {
+    start = false;
+    cactus.classList.remove("animationCactus");
+    clearInterval(cronometro);
+
+}
